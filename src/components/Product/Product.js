8ed995ece0ef264880de3c66c93a3fa1dd24b61e
Kasem <akmanik152gmail.com>
2022-09-25 23:56:39 +0600

    cart updated

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -8,19 +8,18 @@ const Product = (props) => {
      
      return (
           
-          <div className="product-carts">
-               
+         <div className="products">
                <img className="product-photo" src={img} alt="" />
-               <h2>Name: {name} </h2>
-               <h2>Seller: {seller} </h2>
-               <h2>Price: ${price} </h2>
-               <h2>Ratings: {ratings} </h2>
-
-               
+               <div className="product-carts"> 
+                    <h2>Name: {name} </h2>
+                    <h2>Seller: {seller} </h2>
+                    <h2>Price: ${price} </h2>
+                    <h2>Ratings: {ratings} Stars</h2>
+               </div>
                <div className="btn-cart">
-             <button>Add to Carts</button> 
-             </div>
-          </div>
+                    <button>Add to Carts</button> 
+               </div>
+         </div>
          
          
          
